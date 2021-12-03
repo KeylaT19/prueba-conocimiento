8ae679d31111ea4b9e29397020575087cd4ddbca
Keyla_Triana <keyla.triana@nivelics.co>
2021-12-03 09:40:32 -0500

    ajustes de las observaciones

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,25 @@ $('.slider').slick(
 
     }
 );
+
+// SCROLL AL HEADER 
+ $(function() {
+    var elemento = document.getElementById("header");
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 0) {
+        elemento.classList.add('change');
+      } else {
+        elemento.classList.remove('change');
+      }
+    })
+  }); 
+
+ /*  $(document).ready(function(){
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 0) {
+            $('header').addClass('change');
+          } else {
+            $('header').removeClass('change');
+          }
+      });
+  }); */
